73a8d2be40bfdb95b20f64ae2e075de0c54f96ce
VegetarianRacoon <igovinda100@gmail.com>
2019-09-28 18:25:50 +0700

    Added a sitemap and basic description metadata

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -21,6 +21,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 
+    <!-- Metadata -->
+    <meta name="description" content="ITB MUN once again calls upon you to bridge this multipolar world in which we live in, are you up for the challange, delegate?">
+    <!-- END of Metadata -->
+
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
