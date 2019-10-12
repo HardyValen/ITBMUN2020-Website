700ae675c920d0bb435d21d1a9f289fedc019829
HardyValen <hardyvalen@gmail.com>
2019-10-13 02:55:31 +0700

    might overhauling the scss file structure

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,20 +1,37 @@
 @extends("template.main")
 
-@section('title', 'Admin Dashboard - ITBMUN 2020')
-
 @section('content')
 <main>
 <!-- ## 3. Main Hero Section ========= -->
     <section class="dashboard-body">
-        <div class="dashboard-navigation">Hello World</div>
+        <div class="dashboard-topbar">
+            <div class="dashboard-topbar-title">
+                <b>@yield('topbar')</b>
+            </div>
+            <div class="dashboard-topbar-icons">
+                <a href="#"><i class="fas fa-cog"></i></a>
+                <a href="#"><i class="fas fa-sign-out-alt"></i></a>
+            </div>
+        </div>
         <div class="dashboard-container">
-            <div class="dashboard-topbar">
-                <div class="dashboard-topbar-title">
-                    Topbar Title
-                </div>
-                <div class="dashboard-topbar-icons">
-                    <a href="#"><i class="fas fa-cog"></i></a>
-                    <a href="#"><i class="fas fa-sign-out-alt"></i></a>
+            <div class="dashboard-navigation">
+                <div class="dashboard-pills-container">
+                    <a class="dashboard-pill" href="#">
+                        <img src="{{URL::asset('assets/admin/sidenav/delegate.svg')}}" />
+                        <p>Delegate</p>
+                    </a>
+                    <a class="dashboard-pill" href="#">
+                        <img src="{{URL::asset('assets/admin/sidenav/delegation.svg')}}" />
+                        <p>Delegation</p>
+                    </a>
+                    <a class="dashboard-pill" href="#">
+                        <img src="{{URL::asset('assets/admin/sidenav/announcement.svg')}}" />
+                        <p>Announcement</p>
+                    </a>
+                    <a class="dashboard-pill" href="#">
+                        <img src="{{URL::asset('assets/admin/sidenav/press.svg')}}" />
+                        <p>Press</p>
+                    </a>
                 </div>
             </div>
             <div class="dashboard-content">
