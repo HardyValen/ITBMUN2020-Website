1c808e8c0c6e4a4e85e56a075c539ef38f01f2f0
HardyValen <hardyvalen@gmail.com>
2019-10-16 18:22:47 +0700

    Email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="http://itbmun.org">
-            <img src='https://lh3.googleusercontent.com/wYS_Ibsc20WHXuzDRvcJm8PcVmDx49dWZogSIalIM1nmdxDGsyWueidhssiNJmqoIOL_IWllD-zYN_MGJzmKQOjWscZMIiuvylSP5kfDoc4ki44E5ZGalfklEo3vWN-3Bd4nNOkUvg=w2400' />    
+            <img src='https://lh3.googleusercontent.com/WvvuDBxZPh8BBCG2Y5HOIqfpB0ccKSjykvVAnzaH24inpRvFTMthTQzzNNXy5jC1o3Tli5Cs_oiudgUtwepkYqQLKWvMbZ6qJwOUksq3vTvErNOmq6Bw10MFZgQMv--Peb0H6H7fnA=w2400' /> 
         </a>
     </td>
 </tr>                               
